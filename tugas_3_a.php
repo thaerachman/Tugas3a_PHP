1de31050ea86313a425c36eeb30ee903a4135b88
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-07 15:38:31 +0700

    Add files via upload

---
+++ tugas_3_a.php
@@ -0,0 +1,16 @@
+<?php
+echo "Tugas 3a php <br>";
+echo "------------------ <br>";
+$njarak= 360;
+$nkecepatan= 90;
+$menit=60;
+
+// Rumus:
+$waktu =($njarak / $nkecepatan) * $menit;
+
+echo "Jarak Kota A-Z : ".$njarak." Km";
+echo "<br>";
+echo "kecepatan Motor : ".$nkecepatan." Km/jam";
+echo "<br>";
+echo "Waktu Tempuh : ".$waktu." menit";
+?>
